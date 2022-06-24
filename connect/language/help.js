11a7b03f60632540d7bab31a60fc4a15f4ae1be3
kuh6969 <93518896+kuh6969@users.noreply.github.com>
2022-06-24 21:48:06 +0700

    Create help.js

---
+++ connect/language/help.js
@@ -0,0 +1,120 @@
+exports.help = (prefix, author, botname, owner, wib, jangwak) => {
+return `
+_*=>=>=>=>=>=>=>=>=>=>=>*_
+     *BOT WA MULTI DEVICE*
+*_SEMENTARA GINI DULU BANG FITURNYA, YANG PENTING FITUR STIKER & DOWNLOADER WORK YGY_*
+
+_*=>=>=>=>=>=>=>=>=>=>=>*_
+❑ _*MY SERVER BOT INFO*_
+*× Bot Name: ${global.botname}*
+*× My Owner : Kukuh*
+*× Database : MongoDB*
+*× Library : BaileysMD*
+*× Time : ${wib}*
+*× Tanggal : ${jangwak}*
+_*=>=>=>=>=>=>=>=>=>=>=>*_
+
+*∆ [ SIMPEL BOT MENU ] ∆*
+
+❑ *DOWNLOAD MENU*
+ • #tiktok _url_
+ • #ytmp3 _url_
+ • #ytmp4 _url_
+ • #tiktokmp4 _url_
+ • #tiktokmp3 _url_
+ • #mediafire _url_
+ • #play _judul lagu/vid_
+ • #ytplay _judul lagu/vid_
+
+❑ *GROUP MENU*
+ • #delttt
+ • #vote
+ • #tagall
+ • #group
+ • #upvote
+ • #devote
+ • #cekvote
+ • #delvote
+ • #editinfo
+ • #antilink
+ • #kick _@tag_
+ • #add _@628xxxx_
+ • #tictactoe _@tag_
+ • #hidetag _text_
+ • #setname _text_
+ • #setdesc _text_
+ • #demote _@tag_
+ • #promote _@tag_
+ • #setppgrup _<reply image>_
+
+ ❑ *ANIME MENU*
+ • #nekonime
+ • #randomloli
+ • #randomneko
+ • #randomwaifu
+ • #randomanime
+
+ ❑ *CEK AKUN GAME*
+ • #ff _id_
+ • #ml _id|server_
+ • #genshin _id|server_
+
+❑ *FUN MENU*
+ • #math
+ • #tebak
+ • #jadian
+ • #suitpvp
+ • #jodohku
+ • #family100
+
+❑ *CONVERT MENU*
+ • #attp _text_
+ • #tahta _text_
+ • #teksto _text_
+ • #ssweb _https://nama web_
+ • #tourl _<reply image>_
+ • #sticker _<reply image/vid>_
+ • #toimage _<reply sticker>_
+ • #tovideo _<reply sticker>_
+ • #tomp3 _<reply video>_
+ • #smeme _<reply image> dengan caption *text*_
+ • #memegen _<reply image> dengan caption *text|text*_
+
+❑ *SEARCH MENU*
+ • #quotesanime
+ • #ytsearch _text_
+ • #pinterest _text_
+ • #wallpaper _text_
+ • #wikimedia _text_
+
+❑ *NULIS MENU*
+ • #nulis
+ • #foliokiri
+ • #nuliskiri
+ • #nuliskanan
+ • #foliokanan
+
+ ❑ *OWNER MENU*
+ • #join
+ • #bcall
+ • #bcgc
+ • #leave
+ • #getcase
+ • #creategc
+ • #block
+ • #unblock
+ • #broadcast
+ • #prem del 628xxxx
+ • #prem add 628xxxx
+
+ ❑ *OTHERS MENU*
+ • #ping
+ • #limit
+ • #script
+ • #speed
+ • #status
+ • #runtime
+ • #ceklimit
+ • #botstatus
+`
+}
